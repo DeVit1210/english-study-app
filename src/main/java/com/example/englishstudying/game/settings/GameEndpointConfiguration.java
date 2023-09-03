3b14f055c9c441b5d36140502688f0dab2d454bb
DEN <mozolden7@gmail.com>
2023-09-03 17:34:22 +0300

    feat: add game functional-friendly controller class

---
+++ src/main/java/com/example/englishstudying/game/settings/GameEndpointConfiguration.java
@@ -0,0 +1,24 @@
+package com.example.englishstudying.game.settings;
+
+import com.example.englishstudying.game.GameHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class GameEndpointConfiguration {
+    @Bean
+    RouterFunction<ServerResponse> gameEndpoint(GameHandler gameHandler) {
+        return RouterFunctions.route()
+                .nest(RequestPredicates.path("/api/v1/"), builder -> builder
+                        .POST("/start", gameHandler::startGame)
+                        .GET("/getWord/{id}", gameHandler::requestWord)
+                        .POST("/checkAnswer/{id}", gameHandler::checkAnswer)
+                        .POST("/finish/{id}", gameHandler::finishGame)
+                ).build();
+    }
+}
